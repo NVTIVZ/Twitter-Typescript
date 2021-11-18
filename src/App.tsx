84c0fee633bf19432663e718c65aa5d8fcda5407
unknown <nativzgo@gmail.com>
2021-11-18 11:29:16 +0100

    fixing black screen after build

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 
 function App() {
-  return (<BrowserRouter>
-  <Switch>
-    <Route exact path="/">
-      <Login/>
-    </Route>
-    <Route exact path="/home">
-      <Home/>
-    </Route>
-  </Switch>
-  </BrowserRouter>
-  )
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
